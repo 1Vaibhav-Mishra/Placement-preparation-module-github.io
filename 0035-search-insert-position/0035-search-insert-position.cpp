2f7d5d0f7b2fa54e14bc114030a54d95e982bcf4
1Vaibhav-Mishra <90379095+1Vaibhav-Mishra@users.noreply.github.com>
2023-07-18 08:48:33 +0530

    Time: 7 ms (36.52%), Space: 9.5 MB (94.84%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        cout<<__builtin_popcount(13);
+        return (lower_bound(nums.begin(),nums.end(),target)-nums.begin());
+        
+    }
+};
