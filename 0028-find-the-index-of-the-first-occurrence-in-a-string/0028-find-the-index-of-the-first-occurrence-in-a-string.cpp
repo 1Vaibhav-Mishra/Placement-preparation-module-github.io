f0cf367603633a2778caf47d6bf71e1272be8347
1Vaibhav-Mishra <90379095+1Vaibhav-Mishra@users.noreply.github.com>
2023-07-20 11:03:23 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (91.89%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -3,6 +3,7 @@ public:
     int strStr(string h, string needle) {
        int idx=INT_MAX;
         int n=needle.size();
+          if(h.find(needle) == string :: npos) return -1;
         for(int i=0;i<h.size();i++)
         {
            string s=h.substr(i, n);
